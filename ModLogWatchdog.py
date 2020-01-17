27de8a1c1a19ead57b29a43b38d3c9fefa852e0e
Jimothy <phazonjim@gmail.com>
2020-01-16 23:13:52 -0500

    Functioning prototype

--- ModLogWatchdog.py
+++ ModLogWatchdog.py
@@ -41,22 +41,34 @@ def loadCache():
 
 def getAutoModConfig(reddit):
     #Grab the removal reasons from the wiki
-    wikiPage = reddit.subreddit(config["wiki_subreddit"]).wiki[config["removal_reasons_wiki"]].content_md
-    return yaml.load(wikiPage, Loader=yaml.FullLoader)
+    wikiPage = reddit.subreddit(config["wiki_subreddit"]).wiki[config["wiki_page"]].content_md
+    return yaml.load_all(wikiPage, Loader=yaml.FullLoader)
 
 def saveCache(postCache):
     with open(CACHE_FILE, "w") as fout:
         for chunk in json.JSONEncoder().iterencode(postCache):
             fout.write(chunk)
 
+def check_log(log, action_reasons):
+    for action_reason in action_reasons:
+        if action_reason in log.details:
+            return True
+
 def post_webhook(log):
+    item_id = log.target_fullname.split("_")[1]
+    is_submission = log.action == 'removelink'
     message = ''
-    message += ('Removal Reason: ' + log.details + '\n')
-    message += ('Comment Body: ' + textwrap.shorten(log.target_body, width=1000, placeholder="...(Too long to preview full content)...") + '\n')
-    message += ('Author: ' + log.target_author + '\n')
-    message += ('Permalink: ' + 'www.reddit.com' + log.target_permalink + '\n')
-    webhook = DiscordWebhook(url=config["webhook"], content=message)
-    webhook.execute()
+    message += ('**Removal Reason:** {}\n'.format(log.details))
+    if is_submission:
+        message += ('**Submission Title:** {}\n'.format(log.target_title))
+    if log.target_body:
+        message += ('**Content:** {}\n'.format(textwrap.shorten(log.target_body, width=1000, placeholder="...(Too long to preview full content)...")))
+    message += ('**Author:** {}\n'.format(log.target_author))
+    message += ('**Pushshift Link**: https://api.pushshift.io/reddit/{}/search?subreddits={}&ids={}\n'.format("submission" if is_submission else "comment", config["subreddit"], item_id) )
+    message += ('**Permalink:** https://www.reddit.com{}\n'.format(log.target_permalink))
+    print (message)
+    #webhook = DiscordWebhook(url=config["webhook"], content=message)
+    #webhook.execute()
 
 if __name__ == "__main__":
     #Intialize 
@@ -64,18 +76,31 @@ if __name__ == "__main__":
     postCache = loadCache()
     reddit = initReddit()
     moderator = reddit.subreddit(config["subreddit"]).mod
-
-    #removalReasons = getAutoModConfig(reddit)
+    automod_config = getAutoModConfig(reddit)
+    action_reasons = []
+    for rule in automod_config:
+        if rule and "modmail" in rule:
+            if "action_reason" in rule:
+                action_reasons.append(rule["action_reason"])
     #Local vars
     submissions = {}
+    logs = []
     #Only check for removelink actions, grab last X since we dont want to spend too much time grabbing post information
-    for log in moderator.log(action="removecomment",limit=config["mod_log_depth"]):
-        comment_id = log.target_fullname.split("_")[1]
-
+    for log in moderator.log(action="removecomment",mod='AutoModerator',limit=config["mod_log_depth"]):
+        item_id = log.target_fullname.split("_")[1]
+        if item_id not in postCache:
+            if check_log(log, action_reasons):
+                logs.append(log)
+    for log in moderator.log(action="removepost",mod='AutoModerator',limit=config["mod_log_depth"]):
+        item_id = log.target_fullname.split("_")[1]
+        if item_id not in postCache:
+            if check_log(log, action_reasons):
+                logs.append(log)
+    for log in logs:
         post_webhook(log)
-        #if comment_id not in postCache:
-        #comment_object = reddit.comment(id=comment_id)
-        #print (comment_object.)
+        item_id = log.target_fullname.split("_")[1]
+        if item_id not in postCache:
+            postCache.append(item_id)
     #Write out
     if postCache:
         saveCache(postCache)
