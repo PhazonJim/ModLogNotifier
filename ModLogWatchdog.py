83f579048a6e1bac8d9affa8d52f71e408c5787a
Jimothy <phazonjim@gmail.com>
2020-02-29 09:06:04 -0500

    Updates

--- ModLogWatchdog.py
+++ ModLogWatchdog.py
@@ -7,7 +7,6 @@ import yaml
 import textwrap
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
-
 #===Constants===#
 CONFIG_FILE = os.path.join(os.path.dirname(__file__),"config.yaml")
 CACHE_FILE =  os.path.join(os.path.dirname(__file__), "cache.json")
@@ -25,11 +24,6 @@ def loadConfig():
         print("'config.yaml' could not be located. Please ensure 'config.example' has been renamed")
         exit()
 
-def initReddit():
-    client = config["client"]
-    reddit = praw.Reddit(**client)
-    return reddit
-
 def loadCache():
     postCache = {}
     try:
@@ -39,68 +33,51 @@ def loadCache():
         print (e)
     return postCache
 
-def getAutoModConfig(reddit):
-    #Grab the removal reasons from the wiki
-    wikiPage = reddit.subreddit(config["wiki_subreddit"]).wiki[config["wiki_page"]].content_md
-    return yaml.load_all(wikiPage, Loader=yaml.FullLoader)
+def initReddit():
+    client = config["client"]
+    reddit = praw.Reddit(**client)
+    return reddit
 
 def saveCache(postCache):
     with open(CACHE_FILE, "w") as fout:
         for chunk in json.JSONEncoder().iterencode(postCache):
             fout.write(chunk)
 
-def check_log(log, action_reasons):
-    for action_reason in action_reasons:
-        if action_reason in log.details:
-            return True
-
-def post_webhook(log):
-    item_id = log.target_fullname.split("_")[1]
-    is_submission = log.action == 'removelink'
+def post_webhook(log, report):
+    item_id = log.id
+    is_submission = log._submission
     message = ''
-    message += ('**Removal Reason:** {}\n'.format(log.details))
     if is_submission:
         message += ('**Submission Title:** {}\n'.format(log.target_title))
-    if log.target_body:
-        message += ('**Content:** {}\n'.format(textwrap.shorten(log.target_body, width=1000, placeholder="...(Too long to preview full content)...")))
-    message += ('**Author:** {}\n'.format(log.target_author))
-    message += ('**Pushshift Link**: https://api.pushshift.io/reddit/{}/search?subreddits={}&ids={}\n'.format("submission" if is_submission else "comment", config["subreddit"], item_id) )
-    message += ('**Permalink:** https://www.reddit.com{}\n'.format(log.target_permalink))
-    print (message)
-    #webhook = DiscordWebhook(url=config["webhook"], content=message)
-    #webhook.execute()
+    if report:
+        message += ('**Report:** {}\n'.format(textwrap.shorten(report[0], width=1000, placeholder="...(Too long to preview full content)...")))
+    if log.body:
+        message += ('**Content:** {}\n'.format(textwrap.shorten(log.body, width=1000, placeholder="...(Too long to preview full content)...")))
+    message += ('**Author:** {}\n'.format(log.author.name))
+    message += ('**Permalink:** https://www.reddit.com{}\n'.format(log.permalink))
+    webhook = DiscordWebhook(url=config["webhook"], content=message)
+    webhook.execute()
 
 if __name__ == "__main__":
     #Intialize 
     loadConfig()
     postCache = loadCache()
     reddit = initReddit()
-    moderator = reddit.subreddit(config["subreddit"]).mod
-    automod_config = getAutoModConfig(reddit)
-    action_reasons = []
-    for rule in automod_config:
-        if rule and "modmail" in rule:
-            if "action_reason" in rule:
-                action_reasons.append(rule["action_reason"])
+
     #Local vars
     submissions = {}
     logs = []
     #Only check for removelink actions, grab last X since we dont want to spend too much time grabbing post information
-    for log in moderator.log(action="removecomment",mod='AutoModerator',limit=config["mod_log_depth"]):
-        item_id = log.target_fullname.split("_")[1]
-        if item_id not in postCache:
-            if check_log(log, action_reasons):
-                logs.append(log)
-    for log in moderator.log(action="removepost",mod='AutoModerator',limit=config["mod_log_depth"]):
-        item_id = log.target_fullname.split("_")[1]
-        if item_id not in postCache:
-            if check_log(log, action_reasons):
-                logs.append(log)
-    for log in logs:
-        post_webhook(log)
-        item_id = log.target_fullname.split("_")[1]
-        if item_id not in postCache:
-            postCache.append(item_id)
-    #Write out
-    if postCache:
-        saveCache(postCache)
+    while(True):
+        try:
+            for log in reddit.subreddit(config["subreddit"]).mod.stream.reports():
+                item_id = log.id
+                if log.mod_reports:
+                    for report in log.mod_reports:
+                        if config["report_phrase"] in report[0] and item_id not in postCache:
+                            post_webhook(log, report)
+                            postCache.append(item_id)
+                            saveCache(postCache)
+        except:
+            if postCache:
+                saveCache(postCache)
